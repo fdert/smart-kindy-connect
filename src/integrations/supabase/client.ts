e2134dd1b1509e73938392d5a0bb8737372a9a58
fdert <48841998+fdert@users.noreply.github.com>
2025-09-20 19:29:33 +0000

    add officie data base

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -6,8 +6,8 @@ import type { Database } from './types';
 // const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0am9kdWRsbmZhbXZuZXNjdW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MTYwMzgsImV4cCI6MjA3MjA5MjAzOH0.sXV4caS0mPZ_CjEIzgenbCpQYDhT21T5wuYMUPNisFY";
 
 
-const SUPABASE_URL = "https://ytjodudlnfamvnescumu.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0am9kdWRsbmZhbXZuZXNjdW11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1MTYwMzgsImV4cCI6MjA3MjA5MjAzOH0.sXV4caS0mPZ_CjEIzgenbCpQYDhT21T5wuYMUPNisFYssss";
+const SUPABASE_URL = "https://ppqxarwcgpvjkioiunem.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBwcXhhcndjZ3B2amtpb2l1bmVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNjY0MTksImV4cCI6MjA3Mzk0MjQxOX0.PlI26uIviDdZwTThBC4hjSGWbiAeRg2j4bd0CCkGafk";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
